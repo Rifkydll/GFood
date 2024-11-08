5e5119b09a60b9c5e07ad2eb9424b1b9d4f7f0f1
Rifky Dwi LuthfiN <rifkydlllaja@gmail.com>
2024-11-08 21:08:57 +0700

    File Upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GFood Siap Melayani Anda</title>
+  <!-- Ynurm -->
+
+  <!-- boostrap  -->
+  <link rel="stylesheet" href="assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+  
+  <!-- font awesome -->
+  <link rel="stylesheet" href="assets/Font-Awesome-6.x/css/all.min.css">
+  
+  <!-- font google -->
+  <link rel="stylesheet" href="assets/Google-Font/stylesheet.css">
+  
+  <!-- My Style -->
+  <link rel="stylesheet" href="assets/style.css">
+  
+</head>
+<body>
+
+  <!-- navbar -->
+  <section id="navbar">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">GFood</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Promo</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn button" href="#">Contact us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </section>
+  
+  <!-- jumbotron -->
+  <section id="jumbotron">
+    <div class="jumbotron bg-body-tertiary rounded-3">
+      <div class="container py-5">
+        <h1 class="display-5">
+          Rasakan kelezatan daging <span>premium</span><br />dalam bentuk yang
+          <span>berbeda</span>
+        </h1>
+        <button class="btn button" type="button">
+          <i class="fas fa-cart-shopping"></i>
+          Order Now
+        </button>
+      </div>
+    </div>
+  </section>
+
+   <!-- about us -->
+   <section id="aboutus">
+    <div class="container-fluid bg-light p-5">
+      <div class="row text-center">
+        <div class="col-12 p-5">
+          <h2 class="display-4">Mengapa memilih kami?</h2>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-3">
+          <span class="fa-stack fa-2x">
+            <i class="fas fa-circle fa-stack-2x"></i>
+            <i class="fas fa-plate-wheat fa-stack-1x fa-inverse"></i>
+          </span>
+          <h3 class="mt-4">Bahan Premium</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            dolore eveniet provident ad porro soluta cumque molestiae, debitis
+            officiis quasi minus ut nulla accusamus deleniti sint corrupti
+            ipsam, fugiat eos?
+          </p>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-3">
+          
+          <span class="fa-stack fa-2x">
+            <i class="fas fa-circle fa-stack-2x"></i>
+            <i class="fas fa-utensils fa-stack-1x fa-inverse"></i>
+          </span>
+          <h3 class="mt-4">Chef Berpengalaman</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            dolore eveniet provident ad porro soluta cumque molestiae, debitis
+            officiis quasi minus ut nulla accusamus deleniti sint corrupti
+            ipsam, fugiat eos?
+          </p>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-3">
+          <span class="fa-stack fa-2x">
+            <i class="fas fa-circle fa-stack-2x"></i>
+            <i class="fas fa-dollar-sign fa-stack-1x fa-inverse"></i>
+          </span>
+          <h3 class="mt-4">Harga Terjangkau</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            dolore eveniet provident ad porro soluta cumque molestiae, debitis
+            officiis quasi minus ut nulla accusamus deleniti sint corrupti
+            ipsam, fugiat eos?
+          </p>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-3">
+          <span class="fa-stack fa-2x">
+            <i class="fas fa-circle fa-stack-2x"></i>
+            <i class="fas fa-tree-city fa-stack-1x fa-inverse"></i>
+          </span>
+          <h3 class="mt-4">Ramah Lingkungan</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            dolore eveniet provident ad porro soluta cumque molestiae, debitis
+            officiis quasi minus ut nulla accusamus deleniti sint corrupti
+            ipsam, fugiat eos?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- menu -->
+  <section id="menu">
+    <div  class="Menu">
+      <div class="container bg-light P-5">
+        <div class="row text-center">
+        <div class="col-12 p-5">
+          <h2 class="display-4"> Menu Kami</h2>
+        </div>
+
+        <div class="col-12 col-lg-4 mb-4">
+          <div class="card">
+            <img src="assets/img/menu/bakso.webp" class="card-img-top" alt="Bakso">
+              <div class="card-body">
+              <h5 class="card-title">bakso</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-lg-4 mb-4">
+            <div class="card">
+              <img src="assets/img/menu/somay.webp" class="card-img-top" alt="Somay">
+                <div class="card-body">
+                <h5 class="card-title">Somay</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              <div class="card-footer">
+                <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-4 mb-4">
+              <div class="card">
+                <img src="assets/img/menu/tahu.webp" class="card-img-top" alt="Tahu">
+                  <div class="card-body">
+                  <h5 class="card-title">tahu</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="card-footer">
+                  <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-lg-4 mb-4">
+                <div class="card">
+                  <img src="assets/img/menu/tea.webp" class="card-img-top" alt="tea">
+                    <div class="card-body">
+                    <h5 class="card-title">tea</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <div class="card-footer">
+                    <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-lg-4 mb-4">
+                  <div class="card">
+                    <img src="assets/img/menu/coffee.webp" class="card-img-top" alt="coffee">
+                      <div class="card-body">
+                      <h5 class="card-title">coffee</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <div class="card-footer">
+                      <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-12 col-lg-4 mb-4">
+                    <div class="card">
+                      <img src="assets/img/menu/jus.webp" class="card-img-top" alt="juss">
+                        <div class="card-body">
+                        <h5 class="card-title">Juss</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div>
+                      <div class="card-footer">
+                        <a href="#" class="btn button"><i class="fas fa-cart-shopping"></i>Buy Now</a>
+                        </div>
+                      </div>
+                    </div>
+
+          </div>
+        </div>
+    </div>
+  </section>
+  <!-- promo -->
+  <section id="contactus">
+    <div class="container P-5">
+      <div class="row justify-content-center">
+      <div class="col-12 p-5- text-center">
+        <h2 class="display-4"><b> Promo Terbaru</b></h2>
+      </div>
+  <div class="col-12 col-lg-3 mb-4">
+  <div class="card">
+  <img src="assets/img/promo/1.webp" class="card-img-top" elt="Promo 1">
+  </div>
+  </div>
+  <div class="col-12 col-lg-3 mb-4">
+  <div class="card">
+  <img src="assets/img/promo/2.webp" class="card-img-top" elt="Promo 2">
+  </div>
+  </div>
+  <div class="col-12 col-lg-3 mb-4">
+  <div class="card">
+  <img src="assets/img/promo/3.webp" class="card-img-top" elt="Promo 3">
+  </div>
+  </div>
+  <div class="col-12 col-lg-3 mb-4">
+  <div class="card">
+  <img src="assets/img/promo/4.webp" class="card-img-top" elt="Promo 4">
+  </div>
+  </div>
+  </section>
+
+  <!-- contactus -->
+  <section id="contactus">
+  <div class="container p-5">
+    <div class="row justify-content-center">
+      <div class="col-12 p-5 text-center">
+        <h2 class="display-4"><b>contact us</b></h2>
+      </div>
+      <div class="col-10 col-md-6">
+        <form action="kirim.php" method="post">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Nama Lengkap</label>
+            <input type="text" class="form-control" name="nama" id="exampleFormControlInput1" placeholder="Masukkan Nama">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email Address</label>
+            <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="Masukkan Email">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Cacatan</label>
+            <textarea class="form-control" name="saran" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <button class="btn button" type="submit"><i class="fas fa-envelope"></i> kirim saran</button>
+          </div>
+        </div>
+
+        <div class="col-10 col-md-4">
+          <h2 class="mb-3">Alamat</h2>
+          <p><i class="fas fa-map-pin"></i>Jalan Aria Santika</p>
+          <h2 class="mb-3">Contact</h2>
+          <p><i class="fas fa-user"></i>Rifky Dwi Luthfian</p>
+          <p><i class="fa-brands fa-whatsapp"></i>0881025006811</p>
+          <p><i class="fas fa-envelope"></i>rifkydlllaja@gmail.com</p>
+        </div>
+      </form>
+    </div> 
+  </div>
+</section>
+
+  <!-- footer -->
+  <section id="footer">
+ <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a class="footer-brand" href="#">GFood</a>
+      <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2024 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <a class="itemsosmed" href="https://instagram.com/kyy_2.4">
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+      <a class="itemsosmed" href="mailto:rifkydlllaja@gmail.com">
+          <i class="fa-brands fa-google"></i>
+      </a>
+      <a class="itemsosmed" href="https://www.tiktok.com/@kunaonky">  
+        <i class="fa-brands fa-tiktok"></i>
+      </a>
+      <a class="itemsosmed" href="https://www.twitter.com/@dlllaja24">
+          <i class="fa-brands fa-twitter"></i>
+      </a>
+
+    </ul>
+  </footer>
+</div>
+
+  </section>
+
+  <!-- boostrap 3s -->
+    <script src="assets/boostrap-5.3.3-dist/js/boostrap.bundle.min.js"></script>
+</body>
+</html>
